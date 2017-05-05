e2dee8228e3134769a229e3cf94ccb2bb1e736cf
marsoner <lukas.marsoner@gmail.com>
2017-05-05 18:23:56 +0200

    WIP Refactoring read
     - Added all readers to __init__

--- orbkit/read/__init__.py
+++ orbkit/read/__init__.py
@@ -1,2 +1,13 @@
 from orbkit.read.high_level import read
-__all__ = ['read']
+from orbkit.read.molden import read_molden
+from orbkit.read.gamess import read_gamess
+from orbkit.read.gaussian_fchk import read_gaussian_fchk
+from orbkit.read.gaussian_log import read_gaussian_log
+from orbkit.read.aomix import read_aomix
+from orbkit.read.wfx import read_wfx
+from orbkit.read.wfn import read_wfn
+from orbkit.read.cclib import read_with_cclib
+
+__all__ = ['read', 'read_molden', 'read_gamess', 'read_gaussian_fchk',
+           'read_gaussian_log', 'read_aomix', 'read_wfx', 'read_wfn',
+           'read_with_cclib']
