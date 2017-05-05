57a748a746b29ca0762b4c5d45a365f1e1ae8f73
marsoner <lukas.marsoner@gmail.com>
2017-05-05 12:50:27 +0200

    Made divisions work without imports from future
     - units.py now uses saver interger division

--- orbkit/units.py
+++ orbkit/units.py
@@ -13,13 +13,13 @@ _e = 1.6021766208*1e-19 #C
 _me = 9.10938356*1e31 #kg                                               
 _e0 = 8.854187817*1e-12 #F m**(-1)                                      
 _u = 1.660539040*1e-27 #kg
-_c = 299792458 # m s**-1
+_c = 299792458. # m s**-1
                                                                         
 #Derived units                                                          
 _a0 = 4*np.pi*_e0*_hbar**2 / (_me*_e**2) #Bohr radius                   
 _aa = 1e-10 #Angstrom                                                   
 _ha = _hbar**2 / (_me*_a0**2) #Hartree
-_statC = 1 / 2997924580 #Statcoulomb
+_statC = 1. / 2997924580 #Statcoulomb
 _debye = 1e-20*_statC # Debye
                                                                         
 #Conversion factors                                                     
