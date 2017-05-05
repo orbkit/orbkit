adfd861e8f16888e74e0053773d01294f555dc3e
marsoner <lukas.marsoner@gmail.com>
2017-05-05 20:01:53 +0200

    Correct stupid mistake in units test

--- orbkit/test/units.py
+++ orbkit/test/units.py
@@ -9,9 +9,9 @@ ref_ha2ev = 27.211386
 ref_a02aa = 0.529177210
 ref_debye2ae = 0.393456
 
-equal(ha2ev/ev2ha, 1)
-equal(a02aa/aa2a0, 1)
-equal(debye2ae/ae2debye, 1)
+equal(ha2ev*ev2ha, 1)
+equal(a02aa*aa2a0, 1)
+equal(debye2ae*ae2debye, 1)
 
 equal(ha2ev, ref_ha2ev)
 equal(a02aa, ref_a02aa)
