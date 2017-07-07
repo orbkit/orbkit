3abc09ca3450330a6f3063cb2a0ec09c93883c5e
marsoner <lukas.marsoner@gmail.com>
2017-07-07 14:32:34 +0200

    Update units.py
    Adapted unity.py to adhere to Orbkit standart

--- orbkit/units.py
+++ orbkit/units.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy
 
 
 '''
@@ -6,8 +6,10 @@ CODATA recommended values of the fundamental physical constants: 2014
 doi: 10.1103/RevModPhys.88.035009
 '''
 
+pi = numpy.pi
+
 _h = 6.626070040*1e-34 #J s
-_hbar = _h / (2*np.pi)                                                   
+_hbar = _h / (2*pi)                                                   
 _e = 1.6021766208*1e-19 #C                                              
 _me = 9.10938356*1e-31 #kg                                               
 _e0 = 8.854187817*1e-12 #F m**(-1)                                      
@@ -15,7 +17,7 @@ _u = 1.660539040*1e-27 #kg
 _c = 299792458. # m s**-1
                                                                         
 #Derived units                                                          
-_a0 = 4*np.pi*_e0*_hbar**2 / (_me*_e**2) #Bohr radius                   
+_a0 = 4*pi*_e0*_hbar**2 / (_me*_e**2) #Bohr radius                   
 _aa = 1e-10 #Angstrom                                                   
 _ha = _hbar**2 / (_me*_a0**2) #Hartree
 _statC = 1. / 2997924580 #Statcoulomb
