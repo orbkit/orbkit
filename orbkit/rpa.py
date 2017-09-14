a753d7d36769434f009d9f73d1159787ea2d5220
Gunter Hermann <gunter.hermann@fu-berlin.de>
2017-09-14 14:18:21 +0200

    WIP: Implemented a module to calculate RPA spectra.

--- orbkit/rpa.py
+++ orbkit/rpa.py
@@ -1,87 +1,119 @@
-import numpy
+import numpy, h5py
 from orbkit.display import display
-from orbkit.units import pi
+from orbkit.units import pi, c_in_au,ev2ha
 from orbkit.analytical_integrals import get_ao_dipole_matrix,get_ao_overlap
 from orbkit.analytical_integrals import get_mo_overlap_matrix
 
 class RPAClass:
   '''
   '''
-  def spectrum(self,qc,periodic_axis='xyz',q='xyz',broadening=,numproc=4):
+  def __init__(self,qc,hdf5_input=None):
     
-    # Components of spectrum
-    q = ['%s' % i for i in q]
-    periodic_axis = ['%s' % i for i in periodic_axis]
-    non_perdiodic_axis = list(set(q) - set(periodic_axis))
+    self.qc = qc
+    self.hdf5_input = hdf5_input
+    if hdf5_input:
+      fid = hdf5_input if hdf5_input.endswith('.h5') else '%s.h5' % hdf5_input
+      HDF5_file = h5py.File(fid,'r')
+      self.dm = HDF5_file['dm'][...]
+    else:
+      self.dm = []
+      
+
+  def spectrum(self,unit_cell=numpy.identity(3),pbc=[False, False, False],omega=numpy.linspace(0.0,6.0,401)*ev2ha,eta=0.1,hdf5_fid=None,numproc=4):
     
+    display('Calculating RPA absorption spectrum.\n')
+    # Calculate volume, area or length of periodic axes
+    pbc = numpy.array(pbc)
+    unit_cell[numpy.invert(pbc)] = numpy.identity(3)[numpy.invert(pbc)]
+    volume = numpy.linalg.det(unit_cell)
     
     # Get MO energies and coefficients
+    qc = self.qc
     mo_ene = qc.mo_spec.get_eig()
     HOMO = qc.mo_spec.get_homo()
     LUMO = HOMO + 1
-    
-    # Calculate dipole moments in length gauge
-    if non_perdiodic_axis:
-      display('Calculating dipole moments in length gauge.\n')
-      omr = []
-      for c in non_perdiodic_axis:
-        dm_aoom = get_ao_dipole_matrix(qc,component=c)
-        omr.append(get_mo_overlap_matrix(coeffs,coeffs,dm_aoom,numproc=numproc))
-      omr = numpy.array(omr)
+    q = 'xyz'
     
     # Calculate dipole moments in velocity gauge
-    if periodic_axis:
-      display('Calculating dipole moments in velocity gauge.\n')
-      omv = []
-      for c in periodic_axis:
-        aoom = get_ao_overlap(qc.geo_spec,qc.geo_spec,qc.ao_spec,drv=component)
-        omv.append(get_mo_overlap_matrix(qc.mo_spec,qc.mo_spec,aoom,numproc=numproc))
-      omv = numpy.array(omv)
+    dm = self.dm
+    if dm == []:
+      if True in pbc:
+        display('Calculating dipole moments in velocity gauge.\n')
+        for c in q:
+          aoom = get_ao_overlap(qc.geo_spec,qc.geo_spec,qc.ao_spec,drv=c)
+          dm.append(get_mo_overlap_matrix(qc.mo_spec,qc.mo_spec,aoom,numproc=numproc))
+        dm = numpy.array(dm)
+
+      # Calculate dipole moments in length gauge
+      else:
+        display('Calculating dipole moments in length gauge.\n')
+        for c in q:
+          dm_aoom = get_ao_dipole_matrix(qc,component=c)
+          dm.append(get_mo_overlap_matrix(qc.mo_spec,qc.mo_spec,dm_aoom,numproc=numproc))
+        dm = numpy.array(dm)
+    
+    # Calculate RPA signal
+    xx = omega
+    moocc = qc.mo_spec.get_occ()
+    # Calculating spectra
+    signal = numpy.zeros((len(q),len(xx)))
+    for xyz in range(len(q)):
+      for jj in range(LUMO):
+        for kk in range(LUMO,len(mo_ene)):
+          if jj != kk:
+            docc = moocc[jj] - moocc[kk]
+            signal[xyz] += ((4*pi*eta*docc)/volume)*(1/((xx-mo_ene[kk]+mo_ene[jj])**2+(eta**2)))*(dm[xyz,kk,jj]/(mo_ene[kk]-mo_ene[jj]))**2
+            signal[xyz] += (4*pi*eta*(-docc)/volume)*(1/((xx-mo_ene[jj]+mo_ene[kk])**2+(eta**2)))*(dm[xyz,jj,kk]/(mo_ene[jj]-mo_ene[kk]))**2
+  
+    # Calculate absorption spectra
+    for xyz in range(len(q)):
+      signal[xyz] *= volume*xx/c_in_au
       
-    # 
-    
-    return 'lalala'
+    if hdf5_fid and not self.hdf5_input:
+      fid = hdf5_fid if hdf5_fid.endswith('.h5') else '%s.h5' % hdf5_fid
+      f = h5py.File(fid, 'w')
+      f['dm'] = dm
+      f['omega'] = xx
+      f['signal'] = signal
+      f['volume'] = volume
+      f.close()
+
+    return signal
   
-  def 
+  def get_Tij_otos(Erange=numpy.array([0.0,1.0])*ev2ha,pbc=[False, False, False],hdf5_fid=None,numproc=4):
     
+    # Get MO energies and coefficients
+    qc = self.qc
+    mo_ene = qc.mo_spec.get_eig()
+    HOMO = qc.mo_spec.get_homo()
+    LUMO = HOMO + 1
+    q = 'xyz'
     
+    # Calculate dipole moments in velocity gauge
+    dm = self.dm
+    if not dm:
+      if True in pbc:
+        display('Calculating dipole moments in velocity gauge.\n')
+        for c in q:
+          aoom = get_ao_overlap(qc.geo_spec,qc.geo_spec,qc.ao_spec,drv=c)
+          dm.append(get_mo_overlap_matrix(qc.mo_spec,qc.mo_spec,aoom,numproc=numproc))
+        dm = numpy.array(dm)
 
-#L = 18.885*aa2au
-#alpha = 60/180.0*pi
-#A = L**2*numpy.sin(alpha)
-
-
-  ## Arrange excitation energies
-  #mo_ene = numpy.zeros(len(qc.mo_spec))
-  ## Get HOMO and energy of MOs
-  #for ii in range(len(qc.mo_spec)):
-    #mo_ene[ii] = qc.mo_spec[ii]['energy']
-    #if qc.mo_spec[ii]['occ_num'] != 0:
-      #HOMO = ii
-
-  #fid_dm = '/home/jochen19/PHD/sens_mos2/calc_data/mu_v.h5'
-  #HDF5_file = h5py.File(fid_dm,'r')
-  #omv = HDF5_file['%s/omv' % n][...] 
-
-  #xmin = 0.0
-  #xmax = 8.2
-  #sigma = 0.1
-
-  ## Read and calculate absorption spectra
-  #xx = numpy.linspace(xmin,xmax,401)/hartree2eV
-  #sigma /= hartree2eV
-  #display('Calculate absorption spectra for the %s phenol on MoS2\n' % n)
-  ## Calculating spectra
-  #signal = numpy.zeros((len(component),len(xx)))
-  #for xyz in range(len(component)):
-    #for jj in range(HOMO+1):
-      #for kk in range(HOMO+1,len(mo_ene)):
-        #if jj != kk:
-          #signal[xyz] += ((sigma*2)/A)*(1/((xx-mo_ene[kk]+mo_ene[jj])**2+(sigma**2)))*
-          #signal[xyz] += (sigma*(-2)/A)*(1/((xx-mo_ene[jj]+mo_ene[kk])**2+(sigma**2)))*(omv[xyz,jj,kk]/(mo_ene[jj]-mo_ene[kk]))**2
-  
-  ## Calculate absorption spectra
-  #for xyz in range(len(component)):
-    #signal[xyz] *= 4*pi*xx/137
-
-  #def get_oto(self, qc)
+      # Calculate dipole moments in length gauge
+      else:
+        display('Calculating dipole moments in length gauge.\n')
+        for c in q:
+          dm_aoom = get_ao_dipole_matrix(qc,component=c)
+          dm.append(get_mo_overlap_matrix(qc.mo_spec,qc.mo_spec,dm_aoom,numproc=numproc))
+        dm = numpy.array(dm)
+    
+    # Calculating transition dipole matrix
+    Tij = numpy.zeros((LUMO,len(qc.mo_spec)-LUMO))
+    qc = self.qc
+    for xyz in range(len(q)):
+      for jj in range(LUMO):
+        for kk in range(LUMO,len(qc.mo_spec)):
+          if abs(mo_ene[jj] - mo_ene[kk]) >= Erange[0] and abs(mo_ene[jj] - mo_ene[kk]) <= Erange[0]:
+            Tij[xyz] += dm[xyz,jj,kk]
+    
+    return Tij
