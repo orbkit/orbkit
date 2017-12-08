110e0d322f8c24694e7ad304c9e5c3b2662fb1e7
Christian Stemmle <christian.stemmle@fu-berlin.de>
2017-12-08 14:35:32 +0100

    ENH: More parameters vmd_network_creator:
     - add exit flag
     - pass custom commands to vmd script with mo_options
     - remove **kwargs

--- orbkit/output/vmd.py
+++ orbkit/output/vmd.py
@@ -1,29 +1,33 @@
 from orbkit.display import display
 
-def vmd_network_creator(filename,cube_files=None,render=False,iso=(-0.01,0.01),
-                        abspath=False,**kwargs):
+def vmd_network_creator(filename,cube_files=None,render=False,exit=False,
+                        iso=(-0.01,0.01),abspath=False,mo_options=''):
   '''Creates a VMD script file from a list of cube files provided.
-  
+
   **Parameters:**
-  
+
   filename : str
     Contains the base name of the output file.
   cube_files : None or list of str
     Specifies the cube files which serve as input for the VMD script.
     If None, searches the directory for '.cb' and '.cube' files.
   render : bool
-    If True, the VMD script will automatically create '.tga' files for each 
+    If True, the VMD script will automatically create '.tga' files for each
     cube file.
+  exit : bool
+    If True, exit vmd after rendering.
   iso : tuple
     Specifies the isovalue for the blue and the red isosurface, respectively.
   abspath : bool
     If True, the paths of the cube files will be expanded to absolute file paths.
+  vmd_options : strings
+    Further commands to include in vmd script.
   '''
   from os import path,listdir
   import linecache
   from orbkit import vmd_network_draft
   if cube_files is None:
-    display('No list of cube (.cb or .cube) filenames provided. Checking the directory' + 
+    display('No list of cube (.cb or .cube) filenames provided. Checking the directory' +
             ' of the outputfile...')
     cube_files = []
     for fid in listdir(path.dirname(filename)):
@@ -35,7 +39,7 @@ def vmd_network_creator(filename,cube_files=None,render=False,iso=(-0.01,0.01),
     cube_files = [cube_files]
   elif not isinstance(cube_files,list):
     raise IOError('`cube_files` has to be a list of strings.')
-  
+
   title = []
   mo = ''
   for i,f in enumerate(cube_files):
@@ -47,14 +51,17 @@ def vmd_network_creator(filename,cube_files=None,render=False,iso=(-0.01,0.01),
     linecache.clearcache()
     pid = path.abspath(f) if abspath else path.relpath(f,path.dirname(filename))
     mo += vmd_network_draft.mo_string % {
-                                  'c': i, 
+                                  'c': i,
                                   'n1': pid,
-                                  'n2': title, 
-                                  'isored': iso[0], 
-                                  'isoblue': iso[1], 
-                                  'render': '' if render else '#'
+                                  'n2': title,
+                                  'isored': iso[0],
+                                  'isoblue': iso[1],
+                                  'render': '' if render else '#',
+                                  'mo_options' : mo_options,
                                   }
-  
-  f = open('%(f)s.vmd' % {'f': filename},'w')
-  f.write(vmd_network_draft.vmd_string % {'mo':mo})
+
+  with open('%(f)s.vmd' % {'f': filename},'w') as f:
+    f.write(vmd_network_draft.vmd_string % {'mo':mo})
+    if exit:
+      f.write('exit\n')
   f.close()
