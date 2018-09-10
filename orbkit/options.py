3695c2f3802e4456d54ec7e200984c38dfcc2069
Christian Stemmle <christian.stemmle@fu-berlin.de>
2018-09-10 16:28:41 +0200

    FIX: Python3 incompatible print call

--- orbkit/options.py
+++ orbkit/options.py
@@ -390,7 +390,7 @@ def check_options(error=raise_error,display=print_message,
     except ValueError: 
       if len(data) == 1:
         data = data[0]
-        print data.lower()
+        print(data.lower())
         if not any([i != data.lower() for i in ['all_mo','occupied','unoccupied','virtual']]):   
           setattr(thismodule,attr,
                   check_if_exists(data,
