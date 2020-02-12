035b9a3cf402c963819980f0f03472be11820116
Vincent Pohl <pohl@quantum-on-demand.com>
2020-02-12 16:35:39 +0100

    ed

--- setup.py
+++ setup.py
@@ -6,45 +6,61 @@ from os.path import join
 
 import numpy
 
+with open("README.rst", "r") as fh:
+  long_description = fh.read()
+
 setup(
-    name='orbkit',
-    packages=find_packages(),
-    include_package_data=True,
-    version='1.1.0',
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("orbkit.cy_grid",
-                    sources=["orbkit/cy_grid.pyx"],
-                    include_dirs=[numpy.get_include()],
-                    ),
-                   Extension("orbkit.cy_core",
-                    sources=["orbkit/cy_core.pyx",
-                             "orbkit/c_grid-based.c",
-                             "orbkit/c_support.c"],
-                    include_dirs=[numpy.get_include()],
-                    depends=[join('orbkit', '*.h')],
-                    ),
-                   Extension("orbkit.cy_overlap",
-                    sources=["orbkit/cy_overlap.pyx",
-                             "orbkit/c_non-grid-based.c",
-                             "orbkit/c_support.c"],
-                    include_dirs=[numpy.get_include()],
-                    depends=[join('orbkit', '*.h')],
-                    ),
-                   # detCI@ORBKIT
-                   Extension("orbkit.detci.cy_occ_check",
-                    sources=["orbkit/detci/cy_occ_check.pyx"],
-                    include_dirs=[numpy.get_include()],
-                    ),
-                   Extension("orbkit.detci.cy_ci",
-                    sources=["orbkit/detci/cy_ci.pyx"],
-                    extra_compile_args=['-fopenmp'],
-                    extra_link_args=['-fopenmp'],
-                    include_dirs=[numpy.get_include()],
-                    ),
-                   # Libcint
-                   Extension("orbkit.libcint_interface.cy_mo_integrals",
-                    sources=["orbkit/libcint_interface/cy_mo_integrals.pyx"],
-                    include_dirs=[numpy.get_include()],
-                    ),
-                   ],
+  name='orbkit',
+  packages=find_packages(),
+  include_package_data=True,
+  version='1.1.0',
+  description="A Toolbox for Post-Processing Quantum Chemical Wavefunction Data",
+  long_description=long_description,
+  long_description_content_type="text/x-rst",
+  url="https://github.com/orbkit/orbkit",
+  cmdclass={'build_ext': build_ext},
+  ext_modules=[
+    Extension(
+      "orbkit.cy_grid",
+      sources=["orbkit/cy_grid.pyx"],
+      include_dirs=[numpy.get_include()],
+    ),
+    Extension(
+      "orbkit.cy_core",
+      sources=[
+        "orbkit/cy_core.pyx", "orbkit/c_grid-based.c",
+        "orbkit/c_support.c"
+      ],
+      include_dirs=[numpy.get_include()],
+      depends=[join('orbkit', '*.h')],
+    ),
+    Extension(
+      "orbkit.cy_overlap",
+      sources=[
+        "orbkit/cy_overlap.pyx", "orbkit/c_non-grid-based.c",
+        "orbkit/c_support.c"
+      ],
+      include_dirs=[numpy.get_include()],
+      depends=[join('orbkit', '*.h')],
+    ),
+    # detCI@ORBKIT
+    Extension(
+      "orbkit.detci.cy_occ_check",
+      sources=["orbkit/detci/cy_occ_check.pyx"],
+      include_dirs=[numpy.get_include()],
+    ),
+    Extension(
+      "orbkit.detci.cy_ci",
+      sources=["orbkit/detci/cy_ci.pyx"],
+      extra_compile_args=['-fopenmp'],
+      extra_link_args=['-fopenmp'],
+      include_dirs=[numpy.get_include()],
+    ),
+    # Libcint
+    Extension(
+      "orbkit.libcint_interface.cy_mo_integrals",
+      sources=["orbkit/libcint_interface/cy_mo_integrals.pyx"],
+      include_dirs=[numpy.get_include()],
+    ),
+  ],
 )
