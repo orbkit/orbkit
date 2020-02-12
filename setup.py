459f6e741fbaa9c657e4573f6aca904531bee88f
Vincent Pohl <pohl@quantum-on-demand.com>
2020-02-12 17:36:04 +0100

    ENH: Added missing requirements to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from os.path import join
 
 import numpy
 
-with open("README.rst", "r") as fh:
+with open('README.rst', 'r') as fh:
   long_description = fh.read()
 
 setup(
@@ -14,52 +14,55 @@ setup(
   packages=find_packages(),
   include_package_data=True,
   version='1.1.0',
-  description="A Toolbox for Post-Processing Quantum Chemical Wavefunction Data",
+  description='A Toolbox for Post-Processing Quantum Chemical Wavefunction Data',
   long_description=long_description,
-  long_description_content_type="text/x-rst",
-  url="https://github.com/orbkit/orbkit",
+  long_description_content_type='text/x-rst',
+  url='https://github.com/orbkit/orbkit',
   cmdclass={'build_ext': build_ext},
+  entry_points={'console_scripts': ['orbkit = orbkit.main:run_standalone']},
+  install_requires=['numpy', 'scipy', 'matplotlib', 'h5py',
+                    'pytest', 'pytest-xdist'],
   ext_modules=[
     Extension(
-      "orbkit.cy_grid",
-      sources=["orbkit/cy_grid.pyx"],
+      'orbkit.cy_grid',
+      sources=['orbkit/cy_grid.pyx'],
       include_dirs=[numpy.get_include()],
     ),
     Extension(
-      "orbkit.cy_core",
+      'orbkit.cy_core',
       sources=[
-        "orbkit/cy_core.pyx", "orbkit/c_grid-based.c",
-        "orbkit/c_support.c"
+        'orbkit/cy_core.pyx', 'orbkit/c_grid-based.c',
+        'orbkit/c_support.c'
       ],
       include_dirs=[numpy.get_include()],
       depends=[join('orbkit', '*.h')],
     ),
     Extension(
-      "orbkit.cy_overlap",
+      'orbkit.cy_overlap',
       sources=[
-        "orbkit/cy_overlap.pyx", "orbkit/c_non-grid-based.c",
-        "orbkit/c_support.c"
+        'orbkit/cy_overlap.pyx', 'orbkit/c_non-grid-based.c',
+        'orbkit/c_support.c'
       ],
       include_dirs=[numpy.get_include()],
       depends=[join('orbkit', '*.h')],
     ),
     # detCI@ORBKIT
     Extension(
-      "orbkit.detci.cy_occ_check",
-      sources=["orbkit/detci/cy_occ_check.pyx"],
+      'orbkit.detci.cy_occ_check',
+      sources=['orbkit/detci/cy_occ_check.pyx'],
       include_dirs=[numpy.get_include()],
     ),
     Extension(
-      "orbkit.detci.cy_ci",
-      sources=["orbkit/detci/cy_ci.pyx"],
+      'orbkit.detci.cy_ci',
+      sources=['orbkit/detci/cy_ci.pyx'],
       extra_compile_args=['-fopenmp'],
       extra_link_args=['-fopenmp'],
       include_dirs=[numpy.get_include()],
     ),
     # Libcint
     Extension(
-      "orbkit.libcint_interface.cy_mo_integrals",
-      sources=["orbkit/libcint_interface/cy_mo_integrals.pyx"],
+      'orbkit.libcint_interface.cy_mo_integrals',
+      sources=['orbkit/libcint_interface/cy_mo_integrals.pyx'],
       include_dirs=[numpy.get_include()],
     ),
   ],
